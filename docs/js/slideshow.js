9696b8666b795cb95e95ef37d4715844a1d85da2
williamorim <thewilliam89@gmail.com>
2021-02-06 22:10:09 -0300

    arrumando pagina profissional

---
+++ docs/js/slideshow.js
@@ -0,0 +1,33 @@
+// Thumbnail image controls
+
+var slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n, slideshow) {
+  showSlides(slideIndex += n, id = slideshow);
+}
+
+function currentSlide(n, slideshow) {
+  showSlides(slideIndex = n, id = slideshow);
+}
+
+function showSlides(n, id) {
+  var i;
+  var slideshow = document.getElementById(id);
+  var slides = slideshow.getElementsByClassName("mySlides");
+  var dots = slideshow.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function teste(slideshow) {
+  return slideshow
+}
